6f592541dd6e20a7a9b5abd897c3c9bc31e8cfc0
Moiz Sitabkhan <7955428+moiz-conceptlz@users.noreply.github.com>
2023-09-02 13:11:09 +0400

    font-size updates to match tailwindcss

--- src/index.js
+++ src/index.js
@@ -78,13 +78,27 @@ module.exports = require("tailwindcss/plugin")(mainFunction, {
 			'alexandria': ['\'Alexandria\'','\'Helvetica Neue\'','ui-sans-serif','system-ui','sans-serif']
 		},
 		fontSize: {
-			'xs':'0.75rem',
-			'sm':'0.875rem',
-			'base':'1rem',
-			'lg':'1.125rem',
-			'xl':'1.375rem',
-			'2xl':'1.5rem',
-			'3xl':'1.75rem',
+			'xs':['0.75rem',{
+				lineHeight: '1rem'
+			}],
+			'sm':['0.875rem',{
+				lineHeight: '1.25rem'
+			}],
+			'base':['1rem', {
+				lineHeight: '1.5rem'
+			}],
+			'lg':['1.125rem', {
+				lineHeight: '1.75rem'
+			}],
+			'xl':['1.25rem',{
+				lineHeight: '1.75rem'
+			}],
+			'2xl':['1.5rem',{
+				lineHeight: '2rem'
+			}],
+			'3xl':['1.875rem',{
+				lineHeight: '2.25rem'
+			}],
 			'h6':'1.25rem',
 			'h5':'1.625rem',
 			'h4':'2rem',
